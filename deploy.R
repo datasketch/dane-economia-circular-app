f266897b0a9cccf41988cb4dee4ac1b2a25aca2a
CamilaAchuri <camilaachury.4@gmail.com>
2022-09-26 18:31:08 -0300

    add deploy function

---
+++ deploy.R
@@ -0,0 +1,16 @@
+library(rsconnect)
+
+error_on_missing_name <- function(name){
+  var <- Sys.getenv(name, unset=NA)
+  if(is.na(var)){
+    stop(paste0("cannot find ",name, " !"),call. = FALSE)
+  }
+  gsub("\"", '',var)
+}
+# Authenticate
+setAccountInfo(name = error_on_missing_name("SHINY_ACC_NAME"),
+               token = error_on_missing_name("TOKEN"),
+               secret = error_on_missing_name("SECRET"))
+# Deploy the application.
+deployApp(forceUpdate = TRUE,
+          appName = error_on_missing_name("MASTERNAME"))
